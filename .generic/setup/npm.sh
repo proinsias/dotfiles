c6846d49dd0c589ebcd20480929c114b107e0420
Francis T. O'Donovan <proinsias@users.noreply.github.com>
2016-08-24 10:59:07 -0400

    Moving npm and ruby scripts here

---
+++ .generic/setup/npm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+npm install -g wakatimecli # https://www.npmjs.com/package/wakatimecli # https://wakatime.com
+npm install -g htmlhint
+npm install -g csslint
+npm install -g semistandard
+npm install -g standard
+npm install -g git-guilt
+npm install -g eslint
+npm install -g jscs
+npm install -g jshint
+npm install -g jslint
+npm install -g svgo

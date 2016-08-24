0925c3d2fcd202f4485f84b2ba0994aad1776547
Francis T. O'Donovan <francis.odonovan@gmail.com>
2016-08-24 10:55:03 -0400

    Updates to computer setup scripts

---
+++ .generic/setup/ruby-build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "###############################################################################"
+echo "ruby-build"
+echo "###############################################################################"
+
+echo ""
+echo "Installing as rbenv plubin"
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+
--- .osx/setup/pmset.sh
+++ .osx/setup/pmset.sh
@@ -4,15 +4,28 @@ echo "##########################################################################
 echo "pmset"
 echo "###############################################################################"
 
+## Show All Power Management Settings
+#sudo pmset -g
+
+# The -a, -b, -c, -u flags determine whether the settings apply to
+# battery ( -b ), charger (wall power) ( -c ), UPS ( -u ) or all ( -a ).
+
 echo ""
 echo "Speeding up wake from sleep to 24 hours from an hour"
 # http://www.cultofmac.com/221392/quick-hack-speeds-up-retina-macbooks-wake-from-sleep-os-x-tips/
 pmset -a standbydelay 86400
 
-#echo ""
-#echo "Disable computer sleep and stop the display from shutting off"
-#pmset -a sleep 0
+echo ""
+echo "For charger, disable computer sleep"
+pmset -c sleep 0
+echo "For battery, put computer to sleep after 15 minutes of inactivity"
+pmset -c sleep 0
+
+echo ""
+#echo "Disable putting the monitor to sleep"
 #pmset -a displaysleep 0
+echo "Put display to sleep after 15 minutes of inactivity"
+pmset -a displaysleep 15
 
 echo "###############################################################################"
 echo "SSD"

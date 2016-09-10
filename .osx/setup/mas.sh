edf844785b3108c7589c473a31a292ed679bd488
Francis O'Donovan <francis.odonovan@gmail.com>
2016-09-09 20:40:03 -0400

    Update from firefly

---
+++ .osx/setup/mas.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+echo "###############################################################################"
+echo "mas"
+echo "###############################################################################"
+
+echo ""
+echo "Install Mac App Store apps"
+
+#mas install  442963716 # AirBeam
+#mas install  408974874 # BlackjackHD
+#mas install  523620159 # CleanMyDrive 2
+#mas install  529870855 # Color by Numbers - Animals - Free
+#mas install  407992094 # DailyBible
+mas install 1055511498 # Day One
+mas install  642220194 # DeskConnect
+#mas install  997591055 # DinosaurCombo
+#mas install  980169201 # DinosaurDots
+mas install  672206759 # Disk Diag
+#mas install  409366914 # Doodle Hangman Free
+mas install  586862299 # Duplicate Cleaner For iPhoto
+#mas install  512485834 # Elsewhere
+#mas install  409248669 # Fantastic 4 In A Row Free
+#mas install  407719809 # Fireplace
+#mas install  946386240 # FruitsPuzzles
+#mas install  408980954 # GarageBand
+mas install  467939042 # Growl
+#mas install  436979139 # iDisplay Lite
+mas install  408981434 # iMovie
+mas install  408981381 # iPhoto
+#mas install1 018109117 # Install OS X El Capitan
+mas install  409183694 # Keynote
+#mas install  644089060 # Kid's Activity Centre
+#mas install  570169521 # Live Coloring Book
+#mas install  405728848 # McSolitaire
+mas install  451444120 # Memory Clean
+mas install  784801555 # Microsoft OneNote    
+mas install  715768417 # Microsoft Remote Desktop
+mas install  533491939 # Mint QuickView
+#mas install  449597246 # Motion FX
+#mas install  514951692 # NetSpot
+mas install  409203825 # Numbers
+mas install  823766827 # OneDrive
+mas install  409201541 # Pages
+mas install  568494494 # Pocket
+#mas install  408973905 # PokerHD
+#mas install  473939551 # Prices Drop Monitor
+mas install  439845554 # Reeder
+mas install  509148961 # SMARTReporter
+mas install  424470626 # SSH Tunnel Manager
+#mas install  590386474 # SyncTime
+#mas install  449711125 # Tab for Google+
+#mas install  409380380 # Terrific Tic Tac Toe Free
+#mas install  470453599 # Textify Me
+mas install  485812721 # TweetDeck
+mas install  409789998 # Twitter
+mas install  423123087 # UnPlugged
+#mas install  412700130 # Video Converter
+mas install  415139197 # WhiteNoise
+mas install  494803304 # WiFi Explorer
+mas install  497799835 # Xcode
+#mas install  992143732 # ZooIsland
+#mas install  408895572 # ♡ Sudoku Daily

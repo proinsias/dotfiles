89b0e50fd2e364395c5392bdd3f1a27b4e941b72
Francis T. O'Donovan <proinsias@users.noreply.github.com>
2022-05-18 10:03:39 -0400

    More work using stow

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # dotfiles
+
+My personal collection of dotfiles to be managed with
+[GNU Stow](https://www.gnu.org/software/stow/manual/stow.html).
+
+## Install
+
+Simply run this command from your terminal.
+
+```bash
+curl --location --remote-name https://bashhub.com/setup && bash setup
+```
--- bin/stow.sh
+++ bin/stow.sh
@@ -9,37 +9,52 @@ shopt -s failglob             # Cause globs that don't get expanded to cause err
 shopt -s globstar 2>/dev/null # Match all files and zero or more sub-directories.
 
 if type stow > /dev/null 2>&1 ; then
-    # FIXME: Can I move below to base?
     stow --target="${HOME}" \
+        base \
         ansible \
         aspell \
         bash \
+        boto \
         bundle \
         conda \
         direnv \
+        emacs \
         fzf \
+        gdb \
         git \
+        gitlint \
         gnupg \
         htop \
+        hunspell \
         ipython \
         jupyter \
         kitty \
         ksh \
         mog \
+        pylint \
+        rubocop \
+        screen \
         ssh \
         starship \
+        svn \
         tcsh \
         thefuck \
         tmux \
         vim \
+        wget \
         yamllint \
         zsh
 
     # FIXME: GT gitconfig.
-    # FIXME: Move misc scripts to base/.
-    # FIXME: Move scripts directory to base/bin/.local/bin/. MAKE SURE NO SECRETS!
 
     # FIXME: Move ~/.config/ntfy to ntfy/.config/ntfy?
+
+    # FIXME: GT:
+    # etc_hosts
+    # Why doesn't .gitignore get installed? - copy it manually
+    # .bash
+          #.envrc - needs editing
+          #.ntfy.yml.template
 
     # FIXME:
     # if ubuntu
@@ -51,4 +66,8 @@ else
 fi
 
 
+# FIXME: Move scripts directory to base/bin/.local/bin/. MAKE SURE NO SECRETS!
 # FIXME: Make bin script that git clones this repo and then runs stow!
+# FIXME: Make sure .bashrc and .profile will work on any machine.
+# FIXME: Test on ilovemovies and GCS VM machine.
+# FIXME: Add vnc config files from GCS VM machine!

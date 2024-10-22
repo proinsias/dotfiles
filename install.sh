0e9044cebb7bc08edb0eb9addd37aabb293df4b8
Francis T. O'Donovan <proinsias@users.noreply.github.com>
2024-10-22 10:42:09 -0400

    Updates from ilovemovies

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# FIXME: Add instaler for devbox if not present?!
+
 set -o errexit    # Exit on error. Append || true if you expect an error.
 set -o errtrace   # Exit on error inside any functions or subshells.
 set -o noclobber  # Don't allow overwriting files.
@@ -8,6 +10,40 @@ set -o pipefail   # Produce a failure return code if any pipeline command errors
 shopt -s failglob # Cause globs that don't get expanded to cause errors.
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+# Make sure that these directories already exist so stow symlinks files not directories.
+mkdir -p \
+      ~/.bash \
+      ~/.bash_completions \
+      ~/.bundle \
+      ~/.config/archey4 \
+      ~/.config/atuin \
+      ~/.config/broot \
+      ~/.config/cheat \
+      ~/.config/direnv \
+      ~/.config/htop \
+      ~/.config/kitty \
+      ~/.config/neofetch \
+      ~/.config/pet \
+      ~/.config/sourcery \
+      ~/.config/thefuck \
+      ~/.config/tmuxinator \
+      ~/.config/yamllint \
+      ~/.doom.d \
+      ~/.git-template/hooks \
+      ~/.gnupg \
+      ~/.httpie \
+      ~/.hunspell_default \
+      ~/.ipython \
+      ~/.jupyter \
+      ~/.local/bin \
+      ~/.local/share/devbox/global/default \
+      ~/.oh-my-zsh/custom \
+      ~/.ssh \
+      ~/.subversion/auth \
+      ~/.vnc \
+      ~/.zfunc/ \
+      ~/Library/Preferences/glow
 
 if type stow >/dev/null 2>&1; then
     cd "${SCRIPT_DIR}"/

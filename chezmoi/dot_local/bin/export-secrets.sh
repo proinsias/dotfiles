d56c28bdbdec1917e0f26b84290f62b83b337913
Francis T. O'Donovan <proinsias@users.noreply.github.com>
2025-09-30 12:48:28 -0400

    Add chezmoi/.local/bin/export-secrets.sh Update chezmoi/.config/mise/config.toml

---
+++ chezmoi/dot_local/bin/export-secrets.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+MISE_GITHUB_TOKEN="$(op read op://Parents /github.com/Tokens/mise)"
+export MISE_GITHUB_TOKEN
+POP_TO="$(op read op://Parents/Resend/username)"
+export POP_TO
+RESEND_API_KEY="$(op read op://Parents/Resend/API Key)"
+export RESEND_API_KEY
